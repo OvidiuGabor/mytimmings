e1cd3e68fae0849548926508a8cea579903d61f4
ovidiu.gabor91 <ovidiu.gabor91@gmail.com>
2021-03-12 23:23:32 +0200

    Add eventReceive to the calendar and create a function for setting the values to the modal

--- DBContext/DBModel.cs
+++ DBContext/DBModel.cs
@@ -50,48 +50,6 @@ namespace DBContext
             .HasPrecision(18, 0);
         }
 
-
-        //public static string Decrypt(string cypherString, string Securitykey, bool useHasing)
-        //{
-        //    byte[] keyArray;
-        //    byte[] toEncryptArray = Convert.FromBase64String(cypherString);
-
-        //    string key = Securitykey;
-
-        //    if (useHasing)
-        //    {
-        //        //if hashing was used get the hash code with regards to your key  
-        //        MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
-        //        keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
-        //        //release any resource held by the MD5CryptoServiceProvider  
-
-        //        hashmd5.Clear();
-        //    }
-        //    else
-        //    {
-        //        keyArray = UTF8Encoding.UTF8.GetBytes(key);
-        //    }
-
-        //    TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
-        //    //set the secret key for the tripleDES algorithm  
-        //    tdes.Key = keyArray;
-        //    //mode of operation. there are other 4 modes.   
-        //    //We choose ECB(Electronic code Book)  
-
-        //    tdes.Mode = CipherMode.ECB;
-        //    //padding mode(if any extra byte added)  
-        //    tdes.Padding = PaddingMode.PKCS7;
-
-        //    ICryptoTransform cTransform = tdes.CreateDecryptor();
-        //    byte[] resultArray = cTransform.TransformFinalBlock(
-        //                         toEncryptArray, 0, toEncryptArray.Length);
-        //    //Release resources held by TripleDes Encryptor                  
-        //    tdes.Clear();
-        //    //return the Clear decrypted TEXT  
-        //    return UTF8Encoding.UTF8.GetString(resultArray);
-        //}
-
-
         private static string GetConnectionString(string connectionStringName)
         {
             return ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
