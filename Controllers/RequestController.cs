14f2ec55646f4e04806c82ed2f2c15b5b48a7978
OvidiuGabor <ovidiu.gabor91@gmail.com>
2021-04-11 14:50:08 +0300

    Created the controller and classes for the request/

---
+++ Controllers/RequestController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mytimmings.Controllers
+{
+    public class RequestController : Controller
+    {
+        private DBContext.DBModel db = new DBContext.DBModel();
+        // GET: Request
+        public ActionResult Index()
+        {
+
+            //Instantiate the Container
+            Models.Security.User user = GetUserSession();
+            Models.Security.UserSettings userSettings = GetUserSettingsSession();
+
+            if (user ==  null)
+                 return RedirectToAction("index", "home");
+            try
+            {
+                Models.Request.RequestContainerForView model = new Models.Request.RequestContainerForView(user, userSettings);
+                return View(model);
+            }
+            catch (Exception)
+            {
+                //Log the error maybe?!!
+                return RedirectToAction("index", "home");
+            }
+            
+
+           
+        }
+
+
+
+
+
+        #region get the Session Details
+        private Models.Security.UserSettings GetUserSettingsSession()
+        {
+            return Session["UserSettings"] as Models.Security.UserSettings;
+        }
+
+        private Models.Security.User GetUserSession()
+        {
+            return Session["User"] as Models.Security.User;
+        }
+        #endregion
+    }
+}
