859630cb8e2759703f6acf49572f205c80afc4bf
Ovidiu Gabor <ovidiu.gabor91@gmail.com>
2022-01-07 13:55:09 +0200

    Added chart to the dashboard

---
+++ Models/Portal/Dashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mytimmings.Models.Portal
+{
+    public class Dashboard
+    {
+
+        public Models.Security.User user { get; }
+
+
+        public Dashboard(Security.User user)
+        {
+            this.user = user;
+        }
+
+    }
+}
